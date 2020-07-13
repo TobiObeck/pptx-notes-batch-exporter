14acd114a22f107a908a82128d1d467c2807934a
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2020-07-13 20:32:54 +0200

    logging of text working for single file

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-const projectName = "pptx-notes-to-text"
+const projectName = "pptx-notes-to-text";
 const fs = require("fs").promises;
 const path = require("path");
 const presentationsPath = path.join(__dirname, "presentations");
+const xml2js = require("xml2js");
 
-/*
-const filePromise = readFile(presentationsPath, {encoding: 'utf-8'})
-filePromise.then((file)=>{
-
-})
-.catch((error) =>{
-  throw "Directory must be called 'presentations'. " + error
-})
-*/
+// ## presentation
+// ### slide
 
 fs.readdir(presentationsPath, { encoding: "utf8" })
   .then(async (files) => {
@@ -26,9 +20,9 @@ fs.readdir(presentationsPath, { encoding: "utf8" })
   });
 
 function processFiles(files) {
-  const fileName = files[0]//files.forEach((fileName) => {
-    const tempPath = path.join(presentationsPath, fileName);
-    processUnzippedDirectory(tempPath);
+  const fileName = files[0]; //files.forEach((fileName) => {
+  const tempPath = path.join(presentationsPath, fileName);
+  processUnzippedDirectory(tempPath);
   //});
 }
 
@@ -39,18 +33,18 @@ async function processUnzippedDirectory(tempPath) {
     const pathToNotes = path.join(tempPath, "ppt", "notesSlides");
 
     fs.readdir(pathToNotes, { encoding: "utf8" })
-      .then(async (notesSlidesXml) => {
-        console.log(pathToNotes)
-        console.log(notesSlidesXml) // notes of the first presentation
-        //console.log(notesSlidesXml.length) // how many slides with notes?
+      .then(async (notesSlideXmls) => {
+        //console.log(pathToNotes);
+        //console.log(notesSlideXmls); // notes of the first presentation
+        //console.log(notesSlideXmls.length) // how many slides with notes?
 
-        parseNoteSlides(notesSlidesXml)
+        parseNoteSlides(pathToNotes, notesSlideXmls);
       })
       .catch((error) => {
         throw (
-          "Extracting (unzipped) some_presentation.pptx should create"+
-          "folders with a path like "+
-          `'somewhere\\on\\your\\harddrive\\${projectName}\\presentations\\ppt\\notesSlides'.\n`+
+          "Extracting (unzipped) some_presentation.pptx should create" +
+          "folders with a path like " +
+          `'somewhere\\on\\your\\harddrive\\${projectName}\\presentations\\ppt\\notesSlides'.\n` +
           "The notesSlides folder should contain files like 'notesSlide1.xml', 'notesSlide2.xml'...\n" +
           "This folder was not found! Unzip the pptx files properly" +
           error
@@ -59,6 +53,78 @@ async function processUnzippedDirectory(tempPath) {
   }
 }
 
-function parseNoteSlides(notesSlidesXml){
-  notesSlidesXml.
+function parseNoteSlides(pathToNotes, notesSlideXmls) {
+  const i = 1
+  const notesSlideXmlPath = path.join(pathToNotes, notesSlideXmls[i]); // FIRST SLIDE ONLY
+  console.log(notesSlideXmlPath);
+
+  fs.readFile(notesSlideXmlPath, { encoding: "utf-8" })
+    .then(async (xmlString) => {
+      const stat = await fs.lstat(notesSlideXmlPath);
+      if (stat.isFile() === true) {
+        const cleanedXmlString = replaceSpecialCharacters(xmlString);
+        const notesJsObj = await convertXmlToJsObject(cleanedXmlString);
+        const extractedText = extractTextFromJsObj(notesJsObj, notesSlideXmls[i]);
+        console.log(extractedText);
+      }
+    })
+    .catch((error) => {
+      throw error;
+    });
 }
+
+/**
+ * while extracting the pptx files with 7zip special characters 
+ * like & are replaced with an encoded version &amp;
+ * The xml to js object library replace
+ * maybe it would be better to decode the string with
+ * urldecode or decodeURIComponent() or sth like this
+ */
+function replaceSpecialCharacters(xmlString){
+  return xmlString.replace("&amp;", '\&amp;')
+  //return xmlString.replace("&amp;", "'&amp;'")
+  //return xmlString
+}
+
+async function convertXmlToJsObject(xmlString) {
+  try {
+    return await xml2js
+      .parseStringPromise(xmlString /*, options */)
+      .then(function (result) {
+        return result;
+      });
+  } catch (error) {
+    throw error;
+  }
+}
+
+function extractTextFromJsObj(notesJsObj, notesSlideXml) {
+  const paragraphs =
+    notesJsObj["p:notes"]["p:cSld"][0]["p:spTree"][0]["p:sp"][1]["p:txBody"][0][
+      "a:p"
+    ];
+
+  console.log(paragraphs);
+
+  let slideText = `### ${notesSlideXml}\n\n`
+  
+  paragraphs.forEach((para) => {
+    if (para.hasOwnProperty("a:r") === true) {
+      const rows = para["a:r"];
+      const textRow = rows.reduce((acc, row) => acc + row["a:t"][0], "");
+      //console.log("textRow", textRow);
+      slideText += textRow // + '\n'
+    }
+    if (para.hasOwnProperty("a:endParaRPr") === true) {
+       // slideText += '\n'
+    }
+    slideText += '\n'
+  });
+
+  slideText += "\n"
+
+  return slideText
+
+  // //a:r[1]/a:t[1]/text()[1]
+}
+
